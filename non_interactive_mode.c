577039bce6097502476cea358b717895b8ab1501
THOSSYN <alextosin5@gmail.com>
2023-05-26 08:12:56 -0700

    Fix: non_interactive_mode and content

--- non_interactive_mode.c
+++ non_interactive_mode.c
@@ -1,30 +1,30 @@
 #include "main.h"
 
-/**strchr - Locates a character in a string
-* @str: The string to search
-* @c: The character to locate
-*
-* Return: position of the character in the string*
-*/
-char* _strchr(const char* str, char c)
+/**
+ *_strchr - Locates a character in a string
+ *@str: The string to search
+ *@c: The character to locate
+ *
+ *Return: position of the character in the string*
+ */
+char *_strchr(const char *str, char c)
 {
 	while (*str != '\0')
 	{
 		if (*str == c)
 		{
-			return ((char*)str);
+			return ((char *)str);
 		}
 		str++;
 	}
-	    
-	    return (NULL);
+	return (NULL);
 }
 
 /**
-* replaceChar - Replaces a character in a string
-* @str: The string to modify
-*
-*/
+ *replaceChar - Replaces a character in a string
+ *@str: The string to modify
+ *Return: Void
+ */
 void replaceChar(char *str)
 {
 	char *ptr = _strchr(str, '\n');
